71b1f024c5791d44fe2b6bbfe52566f79aa958d4
Skanzer <82811969+Skanzer@users.noreply.github.com>
2024-04-01 15:11:44 +0000

    ft_strcmp.c

---
+++ libft/ft_strcmp.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: szerzeri <szerzeri@42berlin.student.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/01 10:32:26 by szerzeri          #+#    #+#             */
+/*   Updated: 2024/04/01 10:32:58 by szerzeri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	size_t	i;
+
+	i = 0;
+	while (s1[i] == s2[i])
+	{
+		if (s1[i] == '\0' && s2[i] == '\0')
+			return (0);
+		i++;
+	}
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
