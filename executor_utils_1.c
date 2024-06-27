b9686cb378009b0fefb98f31b73994fadb8ec279
Skanzer <82811969+Skanzer@users.noreply.github.com>
2024-06-27 15:38:54 +0000

    actual

--- executor_utils_1.c
+++ executor_utils_1.c
@@ -6,7 +6,7 @@
 /*   By: szerzeri <szerzeri@42berlin.student.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 12:45:57 by szerzeri          #+#    #+#             */
-/*   Updated: 2024/06/06 14:34:28 by szerzeri         ###   ########.fr       */
+/*   Updated: 2024/06/19 19:02:01 by szerzeri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	cmd_execution(t_commands *command, char **env)
 {
 	if (access(command->cmd_path, F_OK | R_OK) == -1)
 	{
-		error_msg(command->cmd_args[0]);
+		error_msg(command->cmd_args[0], "command not found", CMD_NOT_FOUND);
 		free_double(env);
 		exit(CMD_NOT_FOUND);
 	}
